e2deadc3db30ae99b8af6a54bd8069eb1ac9a0af
Oliver Muellerklein <omuellerklein@berkeley.edu>
2018-05-11 13:59:55 -0700

    Dynamic switch required single vs random burn lat/lon form + light header, dark footer

--- public/javascripts/form_script.js
+++ public/javascripts/form_script.js
@@ -11,9 +11,24 @@ $( document ).ready(function() {
         if (document.getElementById('single-burn-radio').checked) {
             document.getElementById('single-burn-coords').style.display = 'block';
             document.getElementById('random-burn-coords').style.display = 'none';
+
+            document.getElementById('ignition-lat').required = true;
+            document.getElementById('ignition-lon').required = true;
+            document.getElementById('lon-min').required = false;
+            document.getElementById('lon-max').required = false;
+            document.getElementById('lat-min').required = false;
+            document.getElementById('lat-max').required = false;
+
         } else if (document.getElementById('random-burn-radio').checked) {
             document.getElementById('single-burn-coords').style.display = 'none';
             document.getElementById('random-burn-coords').style.display = 'block';
+
+            document.getElementById('lon-min').required = true;
+            document.getElementById('lon-max').required = true;
+            document.getElementById('lat-min').required = true;
+            document.getElementById('lat-max').required = true;
+            document.getElementById('ignition-lat').required = false;
+            document.getElementById('ignition-lon').required = false;
         }
     });
 });
